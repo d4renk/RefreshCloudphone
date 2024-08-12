449e3740949ff57844a26500b73f661711055552
skip <64531014+d4renk@users.noreply.github.com>
2024-08-13 01:09:25 +0800

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         RefreshCloudphone
+// @name         Auto Refresh Cloudphone
 // @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  10-20
+// @description  
 // @author
 // @match        https://cloudphoneh5.buy.139.com/*
 // @grant        none
@@ -11,6 +11,34 @@
 (function() {
     'use strict';
 
-    // 10-20
-    setTimeout(() => location.reload(), Math.random() * 10000 + 10000);
+    function refreshPage() {
+        const refreshInterval = Math.random() * 10000 + 10000;
+        let timeLeft = Math.round(refreshInterval / 1000);
+
+        const timerDiv = document.createElement('div');
+        Object.assign(timerDiv.style, {
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            backgroundColor: 'rgba(0, 0, 0, 0.7)',
+            color: 'white',
+            padding: '20px',
+            borderRadius: '10px',
+            zIndex: '1000',
+            fontSize: '20px',
+            pointerEvents: 'none'
+        });
+        document.body.appendChild(timerDiv);
+
+        const updateTimer = setInterval(() => {
+            timerDiv.textContent = `Refreshing in ${timeLeft--} seconds`;
+            if (timeLeft < 0) {
+                clearInterval(updateTimer);
+                location.reload();
+            }
+        }, 1000);
+    }
+
+    refreshPage();
 })();
