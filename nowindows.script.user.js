738fd4a77fb98bf38be79f6e9cb8d46b57ee626a
skip <64531014+d4renk@users.noreply.github.com>
2024-08-15 19:55:08 +0800

    Update nowindows.script.user.js

--- nowindows.script.user.js
+++ nowindows.script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RefreshCloudphone
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.2
 // @description  Refresh every 4-5 minutes
 // @author
 // @match        https://cloudphoneh5.buy.139.com/*
@@ -11,6 +11,12 @@
 (function() {
     'use strict';
 
-    // Refresh every 4-5 minutes
-    setTimeout(() => location.reload(), Math.random() * 60000 + 240000);
+    const refreshPage = () => location.reload(true);
+    const refreshInterval = Math.random() * 60000 + 240000;
+    setTimeout(refreshPage, refreshInterval);
+
+    window.addEventListener('beforeunload', () => {
+        let highestTimeoutId = setTimeout(() => {});
+        while (highestTimeoutId--) clearTimeout(highestTimeoutId);
+    });
 })();
